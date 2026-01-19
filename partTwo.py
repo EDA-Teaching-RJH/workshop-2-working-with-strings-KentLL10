6cbe3e3204b82bd5b6d8bf23a9f3643ce8f8f971
KentLL10 <ll10@kent.ac.uk>
2026-01-19 13:53:55 +0000

    pythag

--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,13 @@
 import math  
 
 def main():
-#TO DO  
+    A = int(input("Input side A as an integer: "))
+    B = int(input("input side B as an integer: ")) #TO DO 
+    C = pythag(A, B)
+    print(C)
 
 def pythag(A,B):
-#TO DO  
-
+    C2 = (A ** 2) + (B ** 2)
+    C = math.sqrt(C2)
+    return C
 main()
